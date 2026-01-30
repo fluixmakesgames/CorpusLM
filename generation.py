f46ea4ca577946061e9c8eaa434469f65861aeae
Charlee Vonlinsowe <spongebobiscool348@gmail.com>
2026-01-29 23:45:27 -0500

    Create generation.py
    Also in a seperate file, this is where most the action happens (I write my projects THEN I make a repo so thats why its alr existent btw)

---
+++ generation.py
@@ -0,0 +1,43 @@
+
+def get_last_word(tokens):
+    if len(tokens) < 2:
+        return ""
+    else:
+        return tokens[len(tokens)-2]
+
+def apply_combos(tokens):
+    if len(tokens) < 2:
+        return tokens
+
+    last_two = tuple(tokens[-2:])
+    if last_two in combines:
+        tokens[-2:] = [combines[last_two]]
+
+    return tokens
+
+def generate_next_word(current_word,last_word):
+    global subject
+    if current_word not in temps:
+        return None
+ 
+    options = temps[current_word]
+    combinations = combines.keys()
+    combo = [last_word,current_word]
+    words = list(options.keys())
+    sub_options = {}
+    for i in options:
+        x = options[i]
+        if len(x) >= 2:
+            if x[1] == subject:
+                sub_options[i] = x
+        else:
+            sub_options[i] = x
+    
+    if debug: print(sub_options, ":", subject, ":", current_word)
+    word = random.choices(list(sub_options.keys()), [val[0] for val in sub_options.values()],k=1)[0]
+    if current_word in subjects:
+        subject = current_word
+    return word
+             return returnstr
+        returnstr += " " + i
+    return returnstr
